4c062cd041c50663921d5491023d4a9771c40468
Wyatt <wyatt.dominguez@endurance.com>
2021-07-09 15:59:42 -0400

    renamed

--- components/Deductions.js
+++ components/Deductions.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import styled from 'styled-components'
-import Item from '../components/Item'
+import Item from './item'
 
 const Layout = styled.div``
 
