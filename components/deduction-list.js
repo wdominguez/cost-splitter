ba5a53ff221b00b15aadff3d30b7db77e23feb88
Wyatt <wyatt.dominguez@endurance.com>
2021-07-12 17:05:44 -0400

    renamed in hopes of fixing bs error

--- components/deduction-list.js
+++ components/deduction-list.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import styled from 'styled-components'
-import Item from './item'
+import Item from './item-deduction'
 
 const Layout = styled.div``
 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import styled from 'styled-components'
-import Deductions from '../components/deductions'
+import Deductions from '../components/deduction-list'
 
 const Layout = styled.div`
   display: flex;
